b2ead5b33a1a80069b79b93d9aaa3724bb0f1bfe
yereong <lily030624@gmail.com>
2026-07-02 16:50:47 +0900

    refactor: JwtTokenProvider 수정
    1. jwt 설정값을 application.yaml로 빼두고, JwtProperties에서 읽어오도록 했습니다.
    
    2. 토큰 생성 및 파싱 로직을 수정하였습니다.

--- src/main/java/kr/adapterz/springboot/auth/JwtTokenProvider.java
+++ src/main/java/kr/adapterz/springboot/auth/JwtTokenProvider.java
@@ -1,55 +1,80 @@
 package kr.adapterz.springboot.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
 import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
 
 @Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
-    private final SecretKey key = Keys.hmacShaKeyFor(
-            "my-super-secret-key-for-jwt-32chars".getBytes(StandardCharsets.UTF_8)
-    );
+    private final JwtProperties jwtProperties;
+    private Key key;
+
+    //스프링 빈이 생성되고 의존성 주입이 끝난 뒤 초기화 로직을 실행할 때 사용하는 어노테이션
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(
+                jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8)
+        );
+    }
 
     //토큰 생성
-    public TokenPair createTokenPair(Long userId) {
-        String accessToken = createToken(userId, 1000 * 60 * 60); // 1시간
-        String refreshToken = createToken(userId, 1000L * 60 * 60 * 24 * 14); // 14일
+    private String createToken(String type, Long userId, Map<String, Object> claims, long expSeconds) {
+        Instant now = Instant.now();
+
+        return Jwts.builder()
+                .subject(String.valueOf(userId))
+                .claim("typ", type)
+                .claims(claims) //jwt 안에 담기는 사용자 정보와 토큰 속성 데이터
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plusSeconds(expSeconds)))
+                .signWith((SecretKey) key, Jwts.SIG.HS256) // 비밀키로 형변환
+                .compact();
+    }
+
+    //토큰쌍 생성
+    public TokenPair createTokenPair(Long userId, String email, String nickname) {
+        String accessToken = createToken("access", userId, Map.of("email", email, "nickname", nickname), jwtProperties.getAccessTokenExpSeconds());
+        String refreshToken = createToken("refresh", userId, Map.of(), jwtProperties.getRefreshTokenExpSeconds());
 
         return new TokenPair(accessToken, refreshToken);
     }
 
-    private String createToken(Long userId, long expirationMillis) {
-        Date now = new Date();
-        Date expiration = new Date(now.getTime() + expirationMillis);
-
-        return Jwts.builder()
-                .subject(String.valueOf(userId))
-                .issuedAt(now)
-                .expiration(expiration)
-                .signWith(key)
-                .compact();
+    //토큰 파싱
+    public Jws<Claims> parse(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) key)
+                .build()
+                .parseSignedClaims(token);
     }
 
+    // 엑세스토큰 여부 확인 메서드
+    public boolean isAccessToken(String token) {
+        return "access".equals(parse(token).getPayload().get("typ", String.class));
+    }
 
-    //토큰 검증
+    // 토큰에서 유저아이디 꺼내오기
     public Long getUserId(String token) {
-        try {
-            String subject = Jwts.parser()
-                    .verifyWith(key)
-                    .build()
-                    .parseSignedClaims(token)
-                    .getPayload()
-                    .getSubject();
-
-            return Long.parseLong(subject);
-        } catch (JwtException | IllegalArgumentException e) {
-            throw new UnauthorizedException();
-        }
+        return Long.valueOf(parse(token).getPayload().getSubject());
     }
+
+    // 토큰 유효기간 단위 밀리초로 바꾸는 메서드
+    public Long getAccessTokenValidityInMilliseconds() {
+        return jwtProperties.getAccessTokenExpSeconds() * 1000;
+    }
+
 }
