6f34d6d72a8afc7e826f4b1639e242bffc04cc63
yereong <lily030624@gmail.com>
2026-08-06 11:40:52 +0900

    fix: allow origin 수정

--- src/main/java/kr/adapterz/springboot/config/WebConfig.java
+++ src/main/java/kr/adapterz/springboot/config/WebConfig.java
@@ -27,10 +27,10 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins(
-				"http://localhost:3000",
-				"http://127.0.0.1:3000",
-				"http://localhost:5500",
-				"http://127.0.0.1:5500"
+				"http://localhost:5173",
+				"http://127.0.0.1:5173",
+				"http://localhost:5173",
+				"http://127.0.0.1:5173"
 			)
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
